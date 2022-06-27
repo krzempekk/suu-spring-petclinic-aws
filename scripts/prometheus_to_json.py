ed3f880cfac50d26acd84e8c773e28bbaa919be2
Hubert Balcerzak <hubertbalc@gmail.com>
2022-06-27 02:02:04 +0200

    add mysql-exporter to prometheus script

--- scripts/prometheus_to_json.py
+++ scripts/prometheus_to_json.py
@@ -6,9 +6,10 @@ import json
 import time
 
 prefixes = ["application_", "container_", "disk_", "envoy_", "executor_", "hikaricp_", "http_", "istio_", "jdbc_",
-            "jvm_", "tomcat_"]
+            "jvm_", "tomcat_", "mysql_"]
 
-app_names = ["customers-service", "vets-service", "visits-service", "api-gateway"]
+app_names = ["customers-service", "vets-service", "visits-service", "api-gateway", "mysql-exporter-customers",
+             "mysql-exporter-vets", "mysql-exporter-visits"]
 
 
 def get_metrics_names(url):
